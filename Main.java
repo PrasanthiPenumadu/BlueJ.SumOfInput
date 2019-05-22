08bf113a409d36f9cd1ada2dccf96ddee19a8a08
Prasanthi Penmadu <prasanthipenmadu@zipcoders-MacBook-Pro-10.local>
2019-05-22 12:00:52 -0400

    Commit the program

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-
+        
+        int sum=0;
+        System.out.println("Enter the number ");
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println("Sum of "+n+" numbers is :"+sum);
     }
 }
